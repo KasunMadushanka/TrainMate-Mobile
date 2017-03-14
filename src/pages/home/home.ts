4e1d850c0e8b98a576f502fcaba248d3fdd2efe9
Kasun <kmadushanka40@gmail.com>
2017-03-14 10:40:06 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LocationTracker } from '../../providers/location-tracker';
+import {Http} from '@angular/http';
+import * as io from "socket.io-client";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+public position=0;
+
+  constructor(public navCtrl: NavController, public locationTracker: LocationTracker) {
+
+  }
+
+  start(){
+    var latitude=this.locationTracker.startTracking();
+
+  var socket=io.connect('http://trainmate16.azurewebsites.net:80');
+
+  socket.on('connect', function () {
+       socket.emit('message',latitude);
+     });
+  }
+
+  stop(){
+    this.locationTracker.stopTracking();
+  }
+
+}
